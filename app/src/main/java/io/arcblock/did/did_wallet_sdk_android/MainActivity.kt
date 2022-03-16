f335d9b51243cf2b538c0a5918b4ccf9c1b8c77f
paper <paperh901230@gmail.com>
2022-03-16 16:28:51 +0800

    init

---
+++ app/src/main/java/io/arcblock/did/did_wallet_sdk_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.arcblock.did.did_wallet_sdk_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
